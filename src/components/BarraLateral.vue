fe0df47c633d9685713452645aa512e684b314de
Gustavo Ribeiro de Oliveira <gustavo.r@inatel.com>
2024-10-28 08:18:51 -0300

    Criação de rotas e pagina de projetos

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -4,6 +4,20 @@
             <img src="../assets/logo.png" alt="">
         </h1>
         <button class="button" @click="alterarTema">{{ textoBotao }}</button>
+        <nav class="panel mt-5">
+          <ul>
+            <li>
+              <router-link to="/" class="link">
+                <i class="fas fa-tasks"></i>
+                Tarefas
+              </router-link>
+              <router-link to="/projetos" class="link">
+                <i class="fas fa-project-diagram"></i>
+                Projetos
+              </router-link>
+            </li>
+          </ul>
+        </nav>
     </header>
 </template>
 
@@ -48,5 +62,17 @@ header {
     padding: 2.5rem;
     height: auto;
   }
+}
+.panel li {
+    margin: 8px 0;
+}
+.link {
+    color: #fff;
+}
+.link:hover {
+    color: #FAF0CA;
+}
+.link.router-link-active {
+    color: #FAF0CA;
 }
 </style>
