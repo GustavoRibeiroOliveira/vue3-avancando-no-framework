27b69c90b25d5ee0f47ba81a64833f04db373c78
Gustavo Ribeiro de Oliveira <gustavo.r@inatel.com>
2024-10-28 10:51:02 -0300

    Refatorando a tela de Project com o crud tendo rotas aninhadas

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,4 +1,6 @@
 import Projetos from "@/views/Projetos.vue"
+import Formulario from "@/views/Projetos/Formulario.vue"
+import Lista from "@/views/Projetos/Lista.vue"
 import Tarefas from "@/views/Tarefas.vue"
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router"
 
@@ -10,9 +12,26 @@ const rotas: RouteRecordRaw[] = [
     }, 
     {
         path: '/projetos',
-        name: 'Projetos',
-        component: Projetos
-    }
+        component: Projetos,
+        children: [
+            {
+                path: '',
+                name: 'Projetos',
+                component: Lista
+            },
+            {
+                path: 'novo',
+                name: 'Novo projeto',
+                component: Formulario
+            },
+            {
+                path: ':id',
+                name: 'Editar projeto',
+                component: Formulario,
+                props: true
+            }
+        ]
+    } 
 ]
 
 const roteador = createRouter({
